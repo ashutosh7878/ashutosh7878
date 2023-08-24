76a911582bbc3c2966a0f1d5884e7471efdebe76
Ashutosh Yadav <103837303+ashutosh7878@users.noreply.github.com>
2023-08-25 03:11:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,28 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
+
+
+// Function to handle the intersection of elements
+function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // Add the animation class when the element is in the viewport
+            entry.target.classList.add('animate__animated', 'animate__bounceIn');
         } else {
-            $('.navbar').removeClass("sticky");
-        }
-
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
+            // Remove the animation class when the element is out of the viewport
+            entry.target.classList.remove('animate__animated', 'animate__bounceIn');
         }
     });
+}
 
-    // slide-up script
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+// Create an Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null, // Use the viewport as the root
+    threshold: 0.3, // Trigger when 30% of the element is in the viewport
+});
 
-    $('.navbar .menu li a').click(function() {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+// Find the elements with the class 'jump-animation'
+const elementsToAnimate = document.querySelectorAll('.jump-animation');
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Web Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Web Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-        // List of background colors
-        var colors = ["red", "green", "blue", "yellow", "pink", "purple", "orange"];
-
-        // Set a timer to change the background color every 3 seconds
-        setInterval(function() {
-            // Get a random color from the list
-            var color = colors[Math.floor(Math.random() * colors.length)];
-
-            // Change the background color
-            document.body.style.backgroundColor = color;
-        }, 150); // Change every 3 seconds
-    });
-     
+// Observe each element
+elementsToAnimate.forEach((element) => {
+    observer.observe(element);
 });
